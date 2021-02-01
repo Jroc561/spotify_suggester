b88868cd778add0b7464907f6384700506d33f1c
JenFaith <jennifer.faith16@gmail.com>
2021-02-01 15:30:02 -0500

    Updates to HTML syntax stuff

--- suggestor/app.py
+++ suggestor/app.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def main_page():
     """Asks user for their name
-        and returns personalized welcome message
-        before moving to song input.
+    and returns personalized welcome message
+    before moving to song input.
     """
     if request.method == "GET":
-        return "Welcome! What's your hame? []" # user name inserted here
+        return render_template('home.html')
     if request.method == "POST":
-        return "Hi, [Name], let's find some new music."
-  
+        return render_template('greet.html', name=request.form.get("name", "you"))
+    
+@app.route("/music", methods = ["GET", "POST"]) 
+def input(): 
+    return render_template("input_song.html", song=request.form.get("input_song"))
 
-@app.route("/music")
-# inputs favorite song
+    # @app.route("/music")
+    # # inputs favorite song
 
-@app.route("/recommended")
-# returns recommended songs
+    # @app.route("/recommended")
+    # # returns recommended songs
+
